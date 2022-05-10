11aa3cd619649c2c1ac5b8839007d22c7416a531
lenovo <oneshot436@gmail.com>
2022-05-10 16:14:29 +0600

    .

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,36 +1,50 @@
 import AdminPages from "../pages/AdminPages";
 import EditProductPage from "../pages/EditProductPage";
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import ProductList from "../components/crud/ProductList";
 import HomePage from "../pages/HomePage";
 import AuthPage from "../pages/AuthPage";
+import { useAuth } from '../contexts/AuthContextProvider';
+import { ADMIN } from '../helpers/consts';
+import NotFoundPage from "../pages/NotFoundPage";
 
 
 const MainRoutes = () => {
+  const { user } = useAuth();
   const PUBLIC_ROUTES = [
-    {
-      link: '/adminPage',
-      element: <AdminPages/>,
-      id: 1,
-    },{
-      link: '/edit/:id',
-      element: <EditProductPage />,
-      id: 2,
-    },
+
     {
       link: '/products',
       element: <ProductList />,
-      id: 3,
+      id: 1,
     },
     {
       link: '/',
       element: <HomePage />,
-      id: 4,
+      id: 2,
     },
     {
       link: '/auth',
       element: <AuthPage />,
-      id: 5,
+      id: 3,
+    },
+    {
+      link: '*',
+      element: <NotFoundPage />,
+      id: 4,
+    },
+  ];
+
+  const PRIVATE_ROUTES = [
+    {
+      link: '/adminPage',
+      element: <AdminPages />,
+      id: 1,
+    },
+    {
+      link: '/edit/:id',
+      element: <EditProductPage />,
+      id: 2,
     },
   ];
 
@@ -40,6 +54,25 @@ const MainRoutes = () => {
         {PUBLIC_ROUTES.map((item) => (
           <Route path={item.link} element={item.element} key={item.id} />
         ))}
+
+{user
+          ? PRIVATE_ROUTES.map((item) => (
+              <Route
+                path={item.link}
+                element={
+                  user.email === ADMIN ? (
+                    item.element
+                  ) : (
+                    <Navigate replace to="*" />
+                  )
+                }
+                key={item.id}
+              />
+            ))
+          : null}
+
+
+
       </Routes>
     </>
   );
