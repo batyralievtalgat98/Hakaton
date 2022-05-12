3175ab4a1901bb273dbdbc7f9a2162a7fbf770b8
Talgat <batyralievtalgat98@gmail.com>
2022-05-12 16:29:27 +0600

    .

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,6 +10,10 @@ import Paper from '@mui/material/Paper';
 import { useCart } from '../../contexts/CartContextProvider';
 import { Box, Button, Grid, IconButton, Typography } from '@mui/material';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { Link, useNavigate } from 'react-router-dom';
+
+
+import './Cart.css'
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -37,6 +41,7 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 export default function Cart() {
   const { getCart, cart, changeProductCount, deleteCartProduct } = useCart();
 
+  const navigate = useNavigate()
   console.log(cart);
 
   React.useEffect(() => {
@@ -50,33 +55,33 @@ export default function Cart() {
 
   return (
     <TableContainer component={Paper} sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-      <Table  sx={{width: '700px'}} 
+      <Table  sx={{maxWidth: '700px', display:{ xs: 'none', sm: 'table'}}} 
         aria-label="customized table">
         
           <TableRow>
-            <StyledTableCell>Picture</StyledTableCell>
-            <StyledTableCell align="right">Name</StyledTableCell>
-            <StyledTableCell align="right">Type</StyledTableCell>
+            <StyledTableCell id='cartComponent' sx={{padding:0}}>Picture</StyledTableCell>
+            <StyledTableCell sx={{padding:0}} >Name</StyledTableCell>
+            <StyledTableCell  sx={{padding:0}}>Type</StyledTableCell>
 
-            <StyledTableCell align="right">Price</StyledTableCell>
-            <StyledTableCell align="right">Count</StyledTableCell>
-            <StyledTableCell align="right">SubPrice</StyledTableCell>
+            <StyledTableCell  sx={{padding:0}}>Price</StyledTableCell>
+            <StyledTableCell  sx={{padding:0}}>Count</StyledTableCell>
+            <StyledTableCell  sx={{padding:0}}>SubPrice</StyledTableCell>
             
           </TableRow>
         
         <TableBody>
           {cart?.products.map((row) => (
             <StyledTableRow key={row.item.id}>
-              <StyledTableCell component="th" scope="row">
+              <StyledTableCell component="th" scope="row" sx={{padding:0}}>
                 <img src={row.item.picture} alt="" width="70" height="70" />
               </StyledTableCell>
-              <StyledTableCell align="right">{row.item.name}</StyledTableCell>
-              <StyledTableCell align="right">{row.item.type}</StyledTableCell>
+              <StyledTableCell  sx={{padding:0}}>{row.item.name}</StyledTableCell>
+              <StyledTableCell  sx={{padding:0}}>{row.item.type}</StyledTableCell>
 
-              <StyledTableCell align="right">{row.item.price}</StyledTableCell>
+              <StyledTableCell  sx={{padding:0}}>{row.item.price}</StyledTableCell>
 
-              <StyledTableCell align="right">
-                <input
+              <StyledTableCell  sx={{padding:'0px'}}>
+                <input className='cartInput1'
                   type="number"
                   min={1}
                   max={1000}
@@ -87,9 +92,9 @@ export default function Cart() {
                 />
               </StyledTableCell>
 
-              <StyledTableCell align="right">{row.subPrice}</StyledTableCell>
+              <StyledTableCell>{row.subPrice}</StyledTableCell>
 
-              <StyledTableCell align="right">
+              <StyledTableCell >
                 <IconButton onClick={() => deleteCartProduct(row.item.id)}>
                   <DeleteOutlineIcon />
                 </IconButton>
@@ -98,6 +103,31 @@ export default function Cart() {
           ))}
         </TableBody>
       </Table>
+
+      {cart?.products.map((row) => (
+      <Box key={row.item.id} sx={{display:{ xs: 'block', sm: 'none', margin:'30px', fontSize:'20px'}}} > 
+         <img src={row.item.picture} alt="" width="90%" className='cartImage' /><br/>
+        <div className='cartSpan'>{row.item.name}</div><br/>
+        <div className='cartSpan'>{row.item.price}$</div><br/>
+        <div className='cartSpan'> Count<input 
+        className='cartInput '
+                  type="number"
+                  min={1}
+                  max={1000}
+                  value={row.count}
+                  
+                  onChange={(e) =>
+                    changeProductCount(e.target.value, row.item.id)
+                  }/></div>
+                <div className='cartSpan'>SubPrice: {row.subPrice}<IconButton  color={'error'}onClick={() => deleteCartProduct(row.item.id)}>
+                  <DeleteOutlineIcon />
+                </IconButton></div>
+        
+      </Box>
+
+
+      ))}
+
 
         <Grid container 
         sx={{m: '0 10px', 
@@ -117,13 +147,16 @@ export default function Cart() {
           <Typography display='flex' textAlign='center'> <h3>TOTAL {cart?.totalPrice + 10} $</h3></Typography>
           </Box>
 
-            <Button onClick={cartCleaner} 
+            <Button onClick={()=>{cartCleaner();navigate('/payment')}} 
+
             sx={{color: 'red', 
             border: '1px solid black', 
             background: '#3e5e70', 
             display: 'flex', 
             alignSelf: 'flex-end',
-            marginLeft: '30%', maxHeight: '10vh'}}>Buy now</Button>
+            marginLeft: '30%', maxHeight: '10vh'}}
+            >Buy now</Button>
+
         </Grid>
     </TableContainer>
   );
